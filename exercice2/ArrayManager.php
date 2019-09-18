33d5359c88435ec04ba39fe1fc16d1f1d4737842
Mayuko <lotuscitron@gmail.com>
2019-09-18 15:31:37 +0200

    Ajouter modification

--- exercice2/ArrayManager.php
+++ exercice2/ArrayManager.php
@@ -6,32 +6,32 @@ class ArrayManager{
 
 
 	public function __construct($tableauRecuperer){
-		$this->_tableau =$tableauRecuperer;
+		$this->_tableau = $tableauRecuperer;
 	}
 
 	public function getTableau(){
-		$this->_tableau;
+		return $this->_tableau;
 	}
 
 	public function setTableau($nouveauTableau){
-		return $this->_tableau = $nouveauTableau;
+		$this->_tableau = $nouveauTableau;
 	}
 
-	function PlusGrandNombreTrouver($tableau){
-	$nombreCourant;
-	$nombrePlusGrand;
+	public function PlusGrandNombreTrouver($tableau){
+		$nombreCourant;
+		$nombrePlusGrand;
 
-	for ($i = 0; $i < count($this->_tableau) ; $i++) { 
-		$nombreCourant = $this->_tableau[$i];
+		for ($i = 0; $i < count($this->_tableau) ; $i++) { 
+			$nombreCourant = $this->_tableau[$i];
 
-		if ($i == 0) {
-			$nombrePlusGrand = $nombreCourant;
+			if ($i == 0) {
+				$nombrePlusGrand = $nombreCourant;
+			}
+
+			if ($nombreCourant > $nombrePlusGrand) {
+				$nombrePlusGrand = $nombreCourant;
+			}
 		}
-
-		if ($nombreCourant > $nombrePlusGrand) {
-			$nombrePlusGrand = $nombreCourant;
-		}
-	}
 	return "Plus grand nombre de ce tableau est ".$nombrePlusGrand.".";
 	}
 
